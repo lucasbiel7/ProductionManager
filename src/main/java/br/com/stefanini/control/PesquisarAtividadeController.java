89f15a5152fcca3c841e863d86b0c413f9817c95
lucasbiel7 <lucasbiel7@icloud.com>
2018-02-17 23:53:11 -0200

    Atualizar progresso de atividade
    Foi criado o componente dentro das tabelas para controlar o progresso das atividades

--- src/main/java/br/com/stefanini/control/PesquisarAtividadeController.java
+++ src/main/java/br/com/stefanini/control/PesquisarAtividadeController.java
@@ -8,25 +8,34 @@ package br.com.stefanini.control;
 import br.com.stefanini.control.dao.AtividadeDAO;
 import br.com.stefanini.control.dao.ModuloDAO;
 import br.com.stefanini.control.dao.PacoteDAO;
+import br.com.stefanini.control.dao.ProgressoAtividadeDAO;
 import br.com.stefanini.control.dao.ProjetoDAO;
 import br.com.stefanini.model.entity.Atividade;
 import br.com.stefanini.model.entity.Modulo;
 import br.com.stefanini.model.entity.OrdemServico;
 import br.com.stefanini.model.entity.Pacote;
+import br.com.stefanini.model.entity.ProgressoAtividade;
 import br.com.stefanini.model.entity.Projeto;
 import br.com.stefanini.model.enuns.Faturamento;
 import br.com.stefanini.model.enuns.SituacaoAtividade;
+import br.com.stefanini.model.enuns.TipoAtividade;
+import br.com.stefanini.model.util.MessageUtil;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -89,16 +98,16 @@ public class PesquisarAtividadeController implements Initializable {
     private TableColumn<Atividade, String> colDetalhada;
 
     @FXML
-    private TableColumn<Atividade, String> colLevantamento;
+    private TableColumn<Atividade, Atividade> colLevantamento;
 
     @FXML
-    private TableColumn<Atividade, String> colDesenvolvimento;
+    private TableColumn<Atividade, Atividade> colDesenvolvimento;
 
     @FXML
-    private TableColumn<Atividade, String> colHomologacao;
+    private TableColumn<Atividade, Atividade> colHomologacao;
 
     @FXML
-    private TableColumn<Atividade, String> colAcoes;
+    private TableColumn<Atividade, Atividade> colAcoes;
 
     @FXML
     private Label lbTotalEstimada;
@@ -124,14 +133,19 @@ public class PesquisarAtividadeController implements Initializable {
         txAtividade.setText("");
         cbSituacao.getItems().setAll(SituacaoAtividade.values());
         cbFaturamento.getItems().setAll(Faturamento.values());
-
         tvAtividade.getItems().setAll(new AtividadeDAO().pegarTodos());
-
         colId.setCellValueFactory(new PropertyValueFactory<>("id"));
         colOs.setCellValueFactory(new PropertyValueFactory<>("ordemServico"));
         colAtividade.setCellValueFactory(new PropertyValueFactory<>("descricao"));
         colEstimada.setCellValueFactory(new PropertyValueFactory<>("contagemDetalhada"));
         colDetalhada.setCellValueFactory(new PropertyValueFactory<>("contagemEstimada"));
+        colLevantamento.setCellValueFactory((TableColumn.CellDataFeatures<Atividade, Atividade> param1) -> new SimpleObjectProperty<>(param1.getValue()));
+        colDesenvolvimento.setCellValueFactory((TableColumn.CellDataFeatures<Atividade, Atividade> param1) -> new SimpleObjectProperty<>(param1.getValue()));
+        colHomologacao.setCellValueFactory((TableColumn.CellDataFeatures<Atividade, Atividade> param1) -> new SimpleObjectProperty<>(param1.getValue()));
+        colAcoes.setCellValueFactory((TableColumn.CellDataFeatures<Atividade, Atividade> param1) -> new SimpleObjectProperty<>(param1.getValue()));
+        colHomologacao.setCellFactory((TableColumn<Atividade, Atividade> param1) -> new TableCellFases(TipoAtividade.TESTE));
+        colLevantamento.setCellFactory((TableColumn<Atividade, Atividade> param1) -> new TableCellFases(TipoAtividade.LEVANTAMENTO));
+        colDesenvolvimento.setCellFactory((TableColumn<Atividade, Atividade> param1) -> new TableCellFases(TipoAtividade.DESENVOLVIMENTO));
     }
 
     @FXML
@@ -215,5 +229,46 @@ public class PesquisarAtividadeController implements Initializable {
     @FXML
     private void btAdicionarAction() {
         //TODO adicionar
+    }
+
+    private class TableCellFases extends TableCell<Atividade, Atividade> {
+
+        private TipoAtividade tipoAtividade;
+
+        public TableCellFases(TipoAtividade tipoAtividade) {
+            this.tipoAtividade = tipoAtividade;
+        }
+
+        @Override
+        protected void updateItem(Atividade item, boolean empty) {
+            if (empty) {
+                setGraphic(null);
+            } else {
+                Spinner<Double> spDados = new Spinner<>();
+                List<ProgressoAtividade> progressoAtividades = new ProgressoAtividadeDAO().pegarPorAtividadeTipo(item, this.tipoAtividade);
+                double initValue = progressoAtividades.stream().map(ProgressoAtividade::getProgresso).findFirst().orElse(0d);
+                spDados.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(initValue, 100d, initValue, 5d));
+                spDados.valueProperty().addListener((ObservableValue<? extends Double> observable, Double oldValue, Double newValue) -> {
+                    ProgressoAtividade progressoAtividade = new ProgressoAtividade();
+                    progressoAtividade.setAtividade(item);
+                    progressoAtividade.setDataDoProgresso(new Date());
+                    progressoAtividade.setProgresso(newValue);
+                    progressoAtividade.setTipoAtividade(this.tipoAtividade);
+                    if (newValue == 100d) {
+                        if (MessageUtil.confirmMessage("Deseja realmente finalizar essa atividade?")) {
+                            new ProgressoAtividadeDAO().salvar(progressoAtividade);
+                            ((SpinnerValueFactory.DoubleSpinnerValueFactory) spDados.getValueFactory()).setMin(progressoAtividade.getProgresso());
+                        } else {
+                            newValue = oldValue;
+                        }
+                    } else {
+                        new ProgressoAtividadeDAO().salvar(progressoAtividade);
+                        ((SpinnerValueFactory.DoubleSpinnerValueFactory) spDados.getValueFactory()).setMin(progressoAtividade.getProgresso());
+                    }
+                });
+                setGraphic(spDados);
+            }
+        }
+
     }
 }
